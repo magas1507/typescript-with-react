dc0534bd56821835673f3466656107315b526ea5
magas1507 <asmg15.07@gmail.com>
2023-08-29 18:42:30 -0300

    Types for references and observers class 9

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,38 +1,11 @@
-
+import { useRef } from "react"
 
 type Props = { image: string}
 
-//forma explícita 
 export const RandomFox = ({image}: Props): JSX.Element =>{
   
-  return <img width={320} height="auto" src={image} className="roundedBorder"/>
+	const node = useRef<HTMLImageElement>(null)
+  
+	return <img ref={node} width={320} height="auto" src={image} className="roundedBorder"/>
 }
 
-// Forma implícito (JSX:Element)
-// export const RandomFox = () =>{
-//   return <img />
-// }
-
-
-/** Usando tipos de React "FunctionComponent"**
-import type { FunctionComponente } from "react"
-****
-const MyComponent: FunctionComponent = () => {
-	return (
-		<div>
-			<h1>Hola!</h1>
-		</div>
-	)
-}*/
-
-/*// Usando tipos de React "FC"**
-import type { FC } from "react"
-***
-const MyComponent: FC = () => {
-	return (
-		<div>
-			<h1>Hola!</h1>
-		</div>
-	)
-}
-*/

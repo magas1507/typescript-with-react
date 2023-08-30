45244a162bf83b26a09c31eed33d878aa93ea11b
magas1507 <asmg15.07@gmail.com>
2023-08-30 11:35:31 -0300

    Lazy loading with observers class 10

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,11 +1,43 @@
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 type Props = { image: string}
 
 export const RandomFox = ({image}: Props): JSX.Element =>{
-  
-	const node = useRef<HTMLImageElement>(null)
-  
-	return <img ref={node} width={320} height="auto" src={image} className="roundedBorder"/>
+  const node = useRef<HTMLImageElement>(null);
+	const [src, setSrc] = useState("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=");
+
+	useEffect(()=>{
+		//new observe
+		const observer = new IntersectionObserver((entries)=>{
+				entries.forEach((entry) => {
+					//onIntersection
+					if (!entry.isIntersecting || !node.current) {
+						return;
+					}
+				});
+				setSrc(image)
+			})
+			
+			//observe node
+			if (node.current) {	
+				observer.observe(node.current);
+			}
+			
+			
+			//disconnect
+			return () =>{
+				observer.disconnect();
+			}	
+		},[image])	
+		
+	return (
+		<img 
+			ref={node} 
+			width={320} 
+			height="auto" 
+			src={image} 
+			className="mx-auto rounded-md bg-gray-300"
+		/>
+		)
 }
 
